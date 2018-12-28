9deb83bab2b4d173151e260402fe4f61c170c29b
flatnote <flatsnote@gmail.com>
2018-12-29 00:55:29 +0700

    Fix jsx warning #2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,19 +11,19 @@ class Footer extends Component {
                             <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
                                 <ul className="list-inline mb-2">
                                     <li className="list-inline-item">
-                                        <a href="#">About</a>
+                                        <a href="#top">About</a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="#">Contact</a>
+                                        <a href="#top">Contact</a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="#">Terms of Use</a>
+                                        <a href="#top">Terms of Use</a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="#">Privacy Policy</a>
+                                        <a href="#top">Privacy Policy</a>
                                     </li>
                                 </ul>
                                 <p className="text-muted small mb-4 mb-lg-0">&copy; Your Website 2018. All Rights Reserved.</p>
@@ -31,17 +31,17 @@ class Footer extends Component {
                             <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
                                 <ul className="list-inline mb-0">
                                     <li className="list-inline-item mr-3">
-                                        <a href="#">
+                                        <a href="#top">
                                             <i className="fab fa-facebook fa-2x fa-fw"></i>
                                         </a>
                                     </li>
                                     <li className="list-inline-item mr-3">
-                                        <a href="#">
+                                        <a href="#top">
                                             <i className="fab fa-twitter-square fa-2x fa-fw"></i>
                                         </a>
                                     </li>
                                     <li className="list-inline-item">
-                                        <a href="#">
+                                        <a href="#top">
                                             <i className="fab fa-instagram fa-2x fa-fw"></i>
                                         </a>
                                     </li>
